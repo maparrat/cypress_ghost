c521961ee18565eaa890787d842b27d4912148a5
miguel parra <ma.parrat@uniandes.edu.co>
2023-11-25 08:18:44 -0500

    es031

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -7,7 +7,7 @@ Cypress.Commands.add('login', () => {
 
   // Ingresa el nombre de usuario y la contraseña
   cy.get('#identification').type('miguel1999parra@gmail.com'); // Reemplaza 'tu_usuario' con el nombre de usuario deseado
-  cy.get('#password').type('zbyHRuEWC6j.m*_'); // Reemplaza 'tu_contraseña' con la contraseña deseada
+  cy.get('#password').type('zbyHRuEWC6j.m*_a'); // Reemplaza 'tu_contraseña' con la contraseña deseada
 
   // Hacer clic en el botón de inicio de sesión (ajusta el selector según la estructura de tu aplicación)
   cy.get('button[type="submit"]').click();
@@ -18,6 +18,80 @@ Cypress.Commands.add('login', () => {
   // Asegúrate de esperar a que la autenticación se complete antes de continuar
   cy.wait(2000);
 });
+
+describe('Iniciar session [estrategia dato conocido]', () => {
+
+  it('Accede a hacer sigin', () => {
+ // Lógica para realizar la autenticación
+ cy.visit('/'); // Ajusta la ruta según la estructura de tu aplicación
+
+ // Ingresa el nombre de usuario y la contraseña
+ cy.get('#identification').type('miguel1999parra@gmail.com'); // Reemplaza 'tu_usuario' con el nombre de usuario deseado
+ cy.get('#password').type('zbyHRuEWC6j.m*_a'); // Reemplaza 'tu_contraseña' con la contraseña deseada
+
+ // Hacer clic en el botón de inicio de sesión (ajusta el selector según la estructura de tu aplicación)
+ cy.get('button[type="submit"]').click();
+
+ // Verificar que la sesión se haya iniciado correctamente (puedes ajustar esto según tu aplicación)
+ cy.url().should('include', '/dashboard'); // Reemplaza '/dashboard' con la ruta a la que se redirige después de iniciar sesión
+
+ // Asegúrate de esperar a que la autenticación se complete antes de continuar
+ cy.wait(2000);
+
+   
+  });
+});
+describe('Iniciar session [estrategia dato aleatorio ]', () => {
+
+  it('Accede a hacer sigin', () => {
+ // Lógica para realizar la autenticación
+ cy.visit('/'); // Ajusta la ruta según la estructura de tu aplicación
+
+ // Ingresa el nombre de usuario y la contraseña
+ const email = faker.word.words();
+ const password = faker.word.words();
+ cy.get('#identification').type(email); // Reemplaza 'tu_usuario' con el nombre de usuario deseado
+ cy.get('#password').type(password); // Reemplaza 'tu_contraseña' con la contraseña deseada
+
+ // Hacer clic en el botón de inicio de sesión (ajusta el selector según la estructura de tu aplicación)
+ cy.get('button[type="submit"]').click();
+
+ // Verificar que la sesión se haya iniciado correctamente (puedes ajustar esto según tu aplicación)
+ cy.contains('Please fill out the form to sign in.'); // Reemplaza '/dashboard' con la ruta a la que se redirige después de iniciar sesión
+
+ // Asegúrate de esperar a que la autenticación se complete antes de continuar
+ cy.wait(2000);
+
+   
+  });
+});
+
+describe('Iniciar session [estrategia dato aleatorio valido]', () => {
+
+  it('Accede a hacer sigin', () => {
+ // Lógica para realizar la autenticación
+ cy.visit('/'); // Ajusta la ruta según la estructura de tu aplicación
+
+ // Ingresa el nombre de usuario y la contraseña
+ const email = faker.internet.email()
+ cy.get('#identification').type(email); // Reemplaza 'tu_usuario' con el nombre de usuario deseado
+ cy.get('#password').type('zbyHRuEWC6j.m*_a'); // Reemplaza 'tu_contraseña' con la contraseña deseada
+
+ // Hacer clic en el botón de inicio de sesión (ajusta el selector según la estructura de tu aplicación)
+ cy.get('button[type="submit"]').click();
+
+ // Verificar que la sesión se haya iniciado correctamente (puedes ajustar esto según tu aplicación)
+ cy.contains('There is no user with that email address.'); // Reemplaza '/dashboard' con la ruta a la que se redirige después de iniciar sesión
+
+ // Asegúrate de esperar a que la autenticación se complete antes de continuar
+ cy.wait(2000);
+
+   
+  });
+});
+
+
+
 
 
 describe('Invitar a un nuevo miembro del personal nuevo [estrategia dato conocido]', () => {
@@ -40,6 +114,7 @@ describe('Invitar a un nuevo miembro del personal nuevo [estrategia dato conocid
     cy.wait(2000); // Ajusta el tiempo de espera según sea necesario
 
     // Ingresar el correo electrónico en el campo de invitación
+    const email = faker.internet.email()
     cy.get('input[name="email"]').type('miguel.parra@bizagi.com');
 
     // Hacer clic en el botón "Send invitation now"
@@ -125,5 +200,3 @@ describe('Invitar a un nuevo miembro del personal nuevo [estrategia dato aleator
 
   });
 });
-
-
